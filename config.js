233f02171a3a3911465287458b8929e838864052
DacfareiT <103461523+DacfareiT@users.noreply.github.com>
2024-09-03 13:17:49 -0500

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
 ["51906545283", 'DacfareIT', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
-["593968585383"], 
+["380501792163"], 
 ["5492266613038"], 
 ["593968263524"],
 ["16474952517"],   
